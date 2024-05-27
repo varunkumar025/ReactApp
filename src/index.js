eefd0d879b4a5112c9d2092a04f62ffe585c2c7d
Varun Kumar <varun.kumar@kimbal.io>
2024-05-27 18:20:06 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -28,6 +28,6 @@ app.use('/api/FormSubmission',formsubmit );
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
